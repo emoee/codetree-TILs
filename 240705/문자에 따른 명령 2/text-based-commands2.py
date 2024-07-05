08115f440d807bfc2ae8111a26473d689efbc814
Minsun <elinnane0@gmail.com>
2024-07-05 10:25:05 +0900

    Title: 문자에 따른 명령 2, Time: 146ms, Memory: 26MB, Status: Passed - CodeTree

--- 240705/문자에 따른 명령 2/text-based-commands2.py
+++ 240705/문자에 따른 명령 2/text-based-commands2.py
@@ -1,13 +1,12 @@
 test = list(input())
-
-x, y = [0, 1, 0, -1], [1, 0, 1, 0] # 위, 오른, 아래, 왼
+x, y = [1, 0, -1, 0], [0, -1, 0, 1] # 좌, 하, 우, 상
 dx, dy = 0, 0
-dir_number = 0
+dir_number = 3
 for i in range(len(test)):
-    if test[i] == "R":
-        dir_number = (dir_number+1)%4
-    elif test[i] == "L":
+    if test[i] == "L":
         dir_number = (dir_number+3)%4
+    elif test[i] == "R":
+        dir_number = (dir_number+1)%4
     elif test[i] == "F":
         dx, dy = dx+x[dir_number], dy+y[dir_number]
 
