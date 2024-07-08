5087c2df5f308b57852ba95ddadd0deffdc780ea
Minsun <elinnane0@gmail.com>
2024-07-08 10:24:40 +0900

    Title: 되돌아오기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/되돌아오기/come-back.py
+++ 240708/되돌아오기/come-back.py
@@ -22,6 +22,6 @@ for i in range(n):
         if x == 0 and y == 0:
             print(time)
             break
-    
-    if i == n and x != 0 and y != 0:
+            
+    if i+1 == n and x != 0 and y != 0:
         print(-1)
