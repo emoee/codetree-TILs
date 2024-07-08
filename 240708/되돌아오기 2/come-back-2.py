86e0e9c54fbd97c386e81d98ea1659abc0254368
Minsun <elinnane0@gmail.com>
2024-07-08 13:54:28 +0900

    Title: 되돌아오기 2, Time: 155ms, Memory: 26MB, Status: Passed - CodeTree

--- 240708/되돌아오기 2/come-back-2.py
+++ 240708/되돌아오기 2/come-back-2.py
@@ -6,15 +6,17 @@ t = 0
 
 x, y = 0, 0 
 for index, move in enumerate(moves):
-    x, y = x+dxs[t], y+dys[t]
-    if move == 'L':
-        t = t + 1 % 4
+    time += 1
+    if move == 'F':
+        x, y = x+dxs[t], y+dys[t]
+    elif move == 'L':
+        t = (t + 3) % 4
     elif move == 'R':
-        t = t + 1 % 4
+        t = (t + 1) % 4
+        
     if x == 0 and y == 0:
         print(time)
         break
-    
-    time += 1
+
     if len(moves)-1 == index:
         print(-1)
