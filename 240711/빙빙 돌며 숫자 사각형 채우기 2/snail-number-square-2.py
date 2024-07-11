f040fbafa16f0078befa4f528292b6caf38ba87f
Minsun <elinnane0@gmail.com>
2024-07-11 09:51:39 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240711/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 240711/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -5,7 +5,7 @@ board = [[0]*m for _ in range(n)]
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < m
 
-dxs, dys = [1, 0, -1, 0],[0, 1, 0, -1] # 하 우 상 좌
+dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1] # 하 우 상 좌
 x, y = 0, 0
 board[x][y] = 1
 dirNumber = 0
@@ -14,8 +14,8 @@ for i in range(2, n*m+1):
     nx, ny = x+dxs[dirNumber], y+dys[dirNumber]
 
     if not in_range(nx, ny) or board[nx][ny] != 0:
-        dirNumber += 1
-
+        dirNumber = (dirNumber+1) % 4
+        
     x, y = x+dxs[dirNumber], y+dys[dirNumber]
     board[x][y] = i
 
