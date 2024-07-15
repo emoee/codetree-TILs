74d99f8097ac06ffba307c0bb36c04f8ad06e070
Minsun <elinnane0@gmail.com>
2024-07-15 22:33:17 +0900

    Title: 이상한 진수 2, Time: 98ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240715/이상한 진수 2/awkward-digits-2.py
+++ 240715/이상한 진수 2/awkward-digits-2.py
@@ -1,6 +1,13 @@
 a = int(input())
 
-print(int(str(a), 2))
+# print(int(str(a), 2))
 
 strA = list(str(a))
-print(strA)
+
+maxValue = int(str(a), 2)
+for i in range(1, len(strA)):
+    strA[i] = str(1 - int(strA[i]))
+    if int(''.join(strA)) > maxValue:
+        maxValue = int(''.join(strA))
+
+print(int(str(maxValue), 2))
