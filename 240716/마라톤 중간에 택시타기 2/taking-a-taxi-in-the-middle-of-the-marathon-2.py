6cd2fa514ebc219e87a8f9b0c2ff5fb9c82e362b
Minsun <elinnane0@gmail.com>
2024-07-16 10:19:31 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240716/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+board = []
+for i in range(n):
+    board.append(tuple(map(int, input().split())))
+
+minValue = 999
+for check in range(1, n-1):
+    sumValue = 0
+    index = 0
+    for i in range(1, n):
+        if check == i:
+            continue
+        x, y = board[index]
+        nx, ny = board[i]
+
+        sumValue += abs(x-nx) + abs(y-ny)
+
+        index = i
+
+    minValue = min(minValue, sumValue)
+
+
+print(minValue)
