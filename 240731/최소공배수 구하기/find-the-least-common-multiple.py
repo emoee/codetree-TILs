c552afdc52e267805f61f023548f79df0038d343
Minsun <elinnane0@gmail.com>
2024-07-31 22:27:20 +0900

    Title: 최소공배수 구하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240731/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,10 +2,13 @@ n , m = map(int, input().split())
 
 def f(a, b):
     s = 1
+    a1, b1 = a, b
     for i in range(2, min(a,b)):
         if a%i == 0 and b%i == 0:
             s *= i
+            a, b = a//i, b//i
     
+    s *= (a1 // s) * (b1 // s)
     return s
 
 print(f(n, m))
