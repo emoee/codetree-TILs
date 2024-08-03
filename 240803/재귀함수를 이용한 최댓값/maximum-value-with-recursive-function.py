cd11fe43b20bfe9bfb92b28fd5548b68e73e91f6
Minsun <elinnane0@gmail.com>
2024-08-03 22:14:19 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240803/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -4,9 +4,11 @@ s = list(map(int, input().split()))
 def MaxValue(s, m, value):
     if m+1 == n:
         return value
-    
+
     if s[m] > s[m+1]:
         value = s[m]
+    else:
+        value = s[m+1]
     
     return MaxValue(s, m+1, value)
 
