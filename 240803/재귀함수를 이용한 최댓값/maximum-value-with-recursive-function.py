3a1e50a0ae05922ce38fa6993da90f1cd25e66c8
Minsun <elinnane0@gmail.com>
2024-08-03 22:15:37 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240803/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -5,10 +5,8 @@ def MaxValue(s, m, value):
     if m+1 == n:
         return value
 
-    if s[m] > s[m+1]:
+    if s[m] > value:
         value = s[m]
-    else:
-        value = s[m+1]
     
     return MaxValue(s, m+1, value)
 
