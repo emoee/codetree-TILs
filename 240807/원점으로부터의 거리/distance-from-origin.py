f7bea2fa647aa3856e6f25fb26c2389381b9fb4f
Minsun <elinnane0@gmail.com>
2024-08-07 17:04:39 +0900

    Title: 원점으로부터의 거리, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,11 @@
+n = int(input())
+board = []
+for i in range(n):
+    x, y = map(int, input().split())
+    m = abs(0-x) + abs(0-y)
+    board.append((i+1,x,y,m))
+
+board.sort(key=lambda x: x[2])
+
+for b in board:
+    print(b[0])
