a47d474ece78125995dc6086243d4d0dcc98ecc1
Minsun <elinnane0@gmail.com>
2024-08-12 22:51:29 +0900

    Title: 흰검 칠하기, Time: 120ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240812/흰검 칠하기/painting-white-black.py
+++ 240812/흰검 칠하기/painting-white-black.py
@@ -2,30 +2,31 @@ n = int(input())
 MAX_K = 100000
 Rline = [0] * (2 * MAX_K + 1)
 Lline = [0] * (2 * MAX_K + 1)
-line = [""] * (2 * MAX_K + 1)
+line = [0] * (2 * MAX_K + 1)
 
 now = MAX_K
 for i in range(n):
     x, c = map(str, input().split())
     x = int(x)
+
     if c == "L":
-       for j in range(x):
+        for j in range(x):
             now -= 1
-            line[now] = 'w'
+            line[now] = 1
             Lline[now] += 1
     else:
         for j in range(x):
-            now += 1
-            line[now] = 'b'
+            line[now] = 2
             Rline[now] += 1
+            now += 1
 
 gray, white, black = 0, 0, 0
 for k in range(2 * MAX_K + 1):
     if Rline[k] >= 2 and Lline[k] >= 2:
         gray += 1
-    elif line[k] == 'w':
+    elif line[k] == 1:
         white += 1
-    elif line[k] == 'b':
+    elif line[k] == 2:
         black += 1
 
 print(white, black, gray)
