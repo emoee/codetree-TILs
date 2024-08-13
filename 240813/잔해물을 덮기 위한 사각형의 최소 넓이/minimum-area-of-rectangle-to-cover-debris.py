40ad02e20d0276b1318adf2099e7f61f47437ed1
Minsun <elinnane0@gmail.com>
2024-08-13 12:02:19 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 151ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240813/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240813/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,14 +1,16 @@
 board = [[0]*2000 for _ in range(2000)]
 
 x1, y1, x2, y2 = map(int, input().split())
-for j in range(x1, x2+1):
-    for k in range(y1, y2+1):
+x1, y1, x2, y2 = x1+1000, y1+1000, x2+1000, y2+1000
+for j in range(x1, x2):
+    for k in range(y1, y2):
         board[j][k] = 1
 
 
 x1, y1, x2, y2 = map(int, input().split())
-for j in range(x1, x2+1):
-    for k in range(y1, y2+1):
+x1, y1, x2, y2 = x1+1000, y1+1000, x2+1000, y2+1000
+for j in range(x1, x2):
+    for k in range(y1, y2):
         board[j][k] = 0
 
 area = sum(sum(row) for row in board)
