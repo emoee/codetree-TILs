9e918ccfab94bd2435d8147261acfacb76ef512a
Minsun <elinnane0@gmail.com>
2024-08-13 13:32:54 +0900

    Title: 연속되는 수 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240813/연속되는 수 2/continuous-number2.py
+++ 240813/연속되는 수 2/continuous-number2.py
@@ -1,19 +1,16 @@
 n = int(input())
 
 line = [int(input()) for _ in range(n)]
-
-MAXV = max(line)
 cnt = 0
 result = 0
 
-for i in range(n):
-    if MAXV == line[i]:
+for i in range(1, n):
+    if line[i-1] == line[i]:
         cnt += 1
     else:
         result = max(result, cnt)
-        cnt = 0
-
-if n <= 1 or n == cnt:
-    result = cnt
+        cnt = 1
+    
+    result = max(result, cnt)
 
 print(result)
