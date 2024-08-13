b642adafd7b4e5a0db4e1a37d746cb6ada43a6ce
Minsun <elinnane0@gmail.com>
2024-08-13 13:34:19 +0900

    Title: 연속되는 수 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/연속되는 수 2/continuous-number2.py
+++ 240813/연속되는 수 2/continuous-number2.py
@@ -1,16 +1,15 @@
 n = int(input())
 
 line = [int(input()) for _ in range(n)]
-cnt = 1
+cnt = 0
 result = 0
 
-for i in range(1, n):
-    if line[i-1] == line[i]:
+for i in range(n):
+    if i == 0 or line[i-1] == line[i]:
         cnt += 1
     else:
         result = max(result, cnt)
         cnt = 1
-    
     result = max(result, cnt)
 
 print(result)
