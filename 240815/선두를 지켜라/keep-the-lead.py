2e51959585cee348b59822edd48fd43a9d33fd1c
Minsun <elinnane0@gmail.com>
2024-08-15 02:44:35 +0900

    Title: 선두를 지켜라, Time: 154ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 240815/선두를 지켜라/keep-the-lead.py
+++ 240815/선두를 지켜라/keep-the-lead.py
@@ -2,34 +2,44 @@ MAXT = 1000000
 
 n, m = map(int, input().split())
 
-A = [0] * (MAXT + 1)
-B = [0] * (MAXT + 1)
+A = []
+B = []
 
 a, b = 1, 1
 for i in range(n):
     v, t = map(int, input().split())
 
     for j in range(t):
-        A[a] = A[a-1] + v/t
-        a += 1
+        a += v
+        A.append(a)
     
 
 for i in range(m):
     v, t = map(int, input().split())
 
     for j in range(t):
-        B[b] = B[b-1] + v/t
-        b += 1
+        b += v
+        B.append(b)
 
 result = 0
-for i in range(2, len(A)):
+now = ''
+pre = ''
+
+u = min(len(A), len(B))
+for i in range(1, u):
     if A[i] == 0 and B[i] == 0:
         break
-    elif A[i-1] > B[i-1] and A[i] > B[i]:
-        continue
-    elif A[i-1] < B[i-1] and A[i] < B[i]:
-        continue
+
+    if A[i] > B[i]:
+        now = 'A'
+    elif A[i] < B[i]:
+        now = 'B'
     else:
-        result += 1
+        now = pre
+    
+    if now != pre and pre != '':
+        result += 1 
+    
+    pre = now 
 
 print(result)
