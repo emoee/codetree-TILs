2858617f4293e70f480cbb4f93c158b0f013045d
Minsun <elinnane0@gmail.com>
2024-08-15 03:56:14 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240815/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240815/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,21 +1,27 @@
 N, K, P, T = map(int, input().split())
 
-person = [0] * (N)
-person[P - 1] = 1
-time = []
-
-for i in range(T):
-    t, x, y = map(int, input().split())
-    time.append([t, x - 1, y - 1])
-    
-time.sort()
 infect = [False] * N
 infect[P - 1] = True
 
-for i in range(min(K, len(time))):
+# 악수 기록 입력
+time = []
+for _ in range(T):
+    t, x, y = map(int, input().split())
+    time.append([t, x - 1, y - 1])
+
+time.sort()
+shakes = [0] * N
+for i in range(T):
     t, x, y = time[i]
-    if infect[x] or infect[y]:
-        infect[x] = True
+    if infect[x]:
+        shakes[x] += 1
+    if infect[y]:
+        shakes[y] += 1
+    
+    if shakes[x] <= K and infect[x]:
         infect[y] = True
+    if shakes[y] <= K and infect[y]:
+        infect[x] = True
+
 
 print("".join(map(str, map(int, infect))))
