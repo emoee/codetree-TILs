26ed95c2ccda32e4a53634f306fd5c94a49b9c29
Minsun <elinnane0@gmail.com>
2024-08-15 23:26:46 +0900

    Title: 1이 3개 이상 있는 위치, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240815/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240815/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,7 +1,25 @@
 n = int(input())
-board = [] * n
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+board = [0] * n
 
 for i in range(n):
     board[i] = list(map(int, input().split()))
 
-print(board)
+maxValue = 0
+
+dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        for dx, dy in zip(dxs, dys):
+            nx, ny = i+dx, j+dy
+            if in_range(nx, ny) and board[nx][ny] == 1:
+                cnt += 1
+        
+        maxValue = max(maxValue, cnt)
+
+print(maxValue)
