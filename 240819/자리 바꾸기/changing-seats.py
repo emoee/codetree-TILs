87538011e8f186f9a80ab48d8f6e579aa134339d
Minsun <elinnane0@gmail.com>
2024-08-19 00:43:13 +0900

    Title: 자리 바꾸기, Time: 5014ms, Memory: 150MB, Status: Time Limit Exceed - CodeTree

--- 240819/자리 바꾸기/changing-seats.py
+++ 240819/자리 바꾸기/changing-seats.py
@@ -1,22 +1,22 @@
 n, k = map(int, input().split())
-
-person = [0] * (n+1)
-counts = [[0] * (n+1) for _ in range(n+1)]
-
-for i in range(1, n+1):
-    person[i] = i
-    counts[i][i] = 1
+person = list(range(n + 1))
+counts = [set() for _ in range(n+1)]
 
 order = []
-for i in range(k):
+for _ in range(k):
     a, b = map(int, input().split())
     order.append((a,b))
 
-for i in range((k*k)):
-    a, b = order[i%k]
-    person[a], person[b] = person[b], person[a]
-    for j in range(1, n+1):
-        counts[person[j]][j] = 1
+for i in range(1, n + 1):
+    counts[i].add(i)
+
+for _ in range(k):
+    for a, b in order:
+        counts[person[a]].add(a)
+        counts[person[b]].add(b)
+
+        person[a], person[b] = person[b], person[a]
+
 
 for c in range(1, n+1):
-    print(sum(counts[c]))
+    print(len(counts[c]))
