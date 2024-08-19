88ee45e7f63e2a6728b1c463d9ad45fb6e833a46
Minsun <elinnane0@gmail.com>
2024-08-19 23:19:44 +0900

    Title: 아름다운 수열 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240819/아름다운 수열 2/beautiful-sequence-2.py
+++ 240819/아름다운 수열 2/beautiful-sequence-2.py
@@ -8,10 +8,12 @@ for i in range(n-m+1):
     check = [0] * m
     for j in range(i, i+m):
         for k in range(m):
-            if Anumbers[j] == Bnumbers[k]:
+            if check[k] == 0 and Anumbers[j] == Bnumbers[k]:
                 check[k] = 1
                 break
+
     if sum(check) == m:
+    #     print(i, Anumbers[i], check)
         count+=1
 
 print(count)
