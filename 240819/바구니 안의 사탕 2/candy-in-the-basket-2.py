b5179f6cc21dc11439b1b0101e6066c01c84704d
Minsun <elinnane0@gmail.com>
2024-08-19 23:51:19 +0900

    Title: 바구니 안의 사탕 2, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240819/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240819/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,17 +1,17 @@
 n, k = map(int, input().split())
 
-board = [0] * 301
+board = [0] * 401 # k의 범위는 200이니까 +-해서 400...
 for i in range(n):
     a, b = map(int, input().split())
     board[b] += a
 
 maxCandy = 0
 
-for i in range(k, 301-k):
+for i in range(k, 401-k):
     candy = 0
     for j in range(i-k, i+k+1):
         candy += board[j]
-    # print(i, k, candy)
+
     maxCandy = max(candy, maxCandy)
 
 print(maxCandy)
