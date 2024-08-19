78d9a99a8f9ae767ec128cff37968ae351f8b67e
Minsun <elinnane0@gmail.com>
2024-08-19 19:41:46 +0900

    Title: 최고의 13위치 2, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240819/최고의 13위치 2/best-place-of-13-2.py
+++ 240819/최고의 13위치 2/best-place-of-13-2.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 board = [[0]*n for _ in range(n)]
-result = [0] * n
+result = []
 
 for i in range(n):
     board[i] = list(map(int, input().split()))
@@ -12,15 +12,14 @@ def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
 for x in range(n):
-    maxValue = 0
-    for y in range(n):
+    for y in range(0, n, 2):
         count = 0
         for dx, dy in zip(dxs, dys):
             nx, ny = x+dx, y+dy
             if in_range(nx, ny):
                 count += board[nx][ny]
-        maxValue = max(maxValue, count)
-    result[x] = maxValue
+        result.append(count)
+    
 
 result.sort()
 print(result[-1] + result[-2])
