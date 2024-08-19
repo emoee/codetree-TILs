b9ac69dd00e0c7c17efad3b2adecefff5db221a7
Minsun <elinnane0@gmail.com>
2024-08-19 19:49:58 +0900

    Title: 최고의 13위치 2, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

--- 240819/최고의 13위치 2/best-place-of-13-2.py
+++ 240819/최고의 13위치 2/best-place-of-13-2.py
@@ -1,7 +1,6 @@
 n = int(input())
 
 board = [[0]*n for _ in range(n)]
-result = []
 
 for i in range(n):
     board[i] = list(map(int, input().split()))
@@ -11,15 +10,23 @@ dxs, dys = [0, 0, 0], [0, 1, 2]
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
+maxValue = 0
 for x in range(n):
-    for y in range(0, n, 3):
-        count = 0
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x+dx, y+dy
-            if in_range(nx, ny):
-                count += board[nx][ny]
-        result.append(count)
+    for y in range(n-2):
+        for i in range(n):
+            for j in range(n-2):
+                if x == i and abs(y-j) <= 2:
+                    continue
+                count1, count2 = 0, 0
+                for dx, dy in zip(dxs, dys):
+                    nx, ny = x+dx, y+dy
+                    if in_range(nx, ny):
+                        count1 += board[nx][ny]
+                for dx, dy in zip(dxs, dys):
+                    nx, ny = i+dx, j+dy
+                    if in_range(nx, ny):
+                        count2 += board[nx][ny]
+                maxValue = max(maxValue, count1 + count2)
     
 
-result.sort()
-print(result[-1] + result[-2])
+print(maxValue)
