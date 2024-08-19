cc255059e3042d56f3dd971afcbaee64b8a48252
Minsun <elinnane0@gmail.com>
2024-08-19 22:01:26 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240819/G or H 3/G-or-H-3.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+MAXV = 1000
+order = [0] * (MAXV+1)
+for i in range(n):
+    l, c = map(str, input().split())
+    if c == 'G': 
+        order[int(l)] = 1
+    elif c == 'H':
+        order[int(l)] = 2
+
+maxValue = 0
+for i in range(MAXV-k+1):
+    count = 0
+    for j in range(i, i+k+1):
+        count += order[j]
+    maxValue = max(maxValue, count)
+
+print(maxValue)
