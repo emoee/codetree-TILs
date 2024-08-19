85d6b59d7ba0215eac7db46b7c88ee90de8b6929
Minsun <elinnane0@gmail.com>
2024-08-19 20:03:39 +0900

    Title: 특정 수와 근접한 합, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240819/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240819/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -3,11 +3,13 @@ n, s = map(int, input().split())
 numbers = list(map(int, input().split()))
 cha = s
 for i in range(n-1):
-    for j in range(1, n):
+    for j in range(n):
+        if i == j:
+            continue
         sumNumbers = sum(numbers)
         sumNumbers -= (numbers[i] + numbers[j])
         
         ssn = abs(s-sumNumbers)
-        cha = min(ssn, cha)
+        cha = min(cha, ssn)
 
 print(cha)
