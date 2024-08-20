d79c4784b9a798c05e06b4140d958894827b2135
Minsun <elinnane0@gmail.com>
2024-08-20 16:24:07 +0900

    Title: 개발자의 능력 3, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,14 @@
+numbers = list(map(int, input().split()))
+
+def diff(i, j, k):
+    sumA = numbers[i] + numbers[j] + numbers[k]
+    sumB = sum(numbers) - sumA
+    return abs(sumA - sumB)
+
+result = 1000000
+for i in range(4):
+    for j in range(i, 5):
+        for k in range(j, 6):
+            result = min(result, diff(i, j, k))
+
+print(result)
