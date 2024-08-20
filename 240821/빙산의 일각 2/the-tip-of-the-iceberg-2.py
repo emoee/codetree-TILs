1423c3f5cd93746e33908fb3c206e62b1c82e830
Minsun <elinnane0@gmail.com>
2024-08-21 02:31:09 +0900

    Title: 빙산의 일각 2, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240821/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -1,23 +1,26 @@
+def check(heights, number):
+    count = 0
+    T = False
+
+    for h in heights:
+        if h > number:
+            if not T:
+                count += 1
+                T = True
+        else:
+            T = False
+    
+    return count
+
+
 n = int(input())
-board = [[0] * 10001 for _ in range(101)]
+heights = [int(input()) for _ in range(n)]
 
-MAXV = 0
-MINV = 10001
-for i in range(n):
-    t = int(input())
-    for j in range(t):
-        board[i][j] = 1
-
-    MAXV = max(MAXV, t)
-    MINV = min(MINV, t)
-
+MAXH = max(heights)
 result = 0
-for i in range(MINV, MAXV + 1):
-    value = 1
-    for j in range(n-1):
-        if board[j][i] == 0 and board[j][i] != board[j+1][i]:
-            value += 1
-            
+
+for i in range(MAXH + 1):
+    value = check(heights, i)
     result = max(result, value)
 
 print(result)
