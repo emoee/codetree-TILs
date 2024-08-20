7b7b2c49c6f2cf4bb107f9e729b47da1761f2b10
Minsun <elinnane0@gmail.com>
2024-08-21 00:54:03 +0900

    Title: 스승의 은혜 3, Time: 175ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240821/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,18 @@
+n, b = map(int, input().split())
+
+order = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+result = 0
+for i in range(n):
+    total = (order[i][0]/2) + order[i][1]
+    for j in range(n):
+        price = sum(order[j])
+        if total + price <= b:
+            total += price
+        else:
+            result = max(result, j)
+    result = max(result, j)
+print(result)
