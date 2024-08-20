60c1812e3ab992e3abde8e9e768a8e14bdaa71bb
Minsun <elinnane0@gmail.com>
2024-08-20 16:24:59 +0900

    Title: 개발자의 능력 3, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/개발자의 능력 3/ability-of-developer-3.py
+++ 240820/개발자의 능력 3/ability-of-developer-3.py
@@ -7,8 +7,8 @@ def diff(i, j, k):
 
 result = 1000000
 for i in range(4):
-    for j in range(i, 5):
-        for k in range(j, 6):
+    for j in range(i+1, 5):
+        for k in range(j+1, 6):
             result = min(result, diff(i, j, k))
 
 print(result)
