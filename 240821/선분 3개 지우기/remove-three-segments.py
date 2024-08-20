34ec806ec79b057583e754f643f31124d085fc04
Minsun <elinnane0@gmail.com>
2024-08-21 00:48:51 +0900

    Title: 선분 3개 지우기, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/선분 3개 지우기/remove-three-segments.py
+++ 240821/선분 3개 지우기/remove-three-segments.py
@@ -8,20 +8,21 @@ result = 0
     
 for i in range(n):
     for j in range(i+1, n):
-        board = [0] * 101
-        value = True
-        for k in range(n):
-            if k != i and k != j:
-                for l in range(n):
-                    if i == l or j == l or k == l:
-                        continue
-                    x, y = order[l]
-                    for b in range(x, y+1):
-                        board[b] += 1
-                        if board[b] > 1:
-                            value = False
+        for k in range(j+1, n):
+            board = [0] * 101
+            value = True
+            
+            for l in range(n):
+                if i == l or j == l or k == l:
+                    continue
                 
-                if value == True:
-                    result += 1
+                x, y = order[l]
+                for b in range(x, y+1):
+                    board[b] += 1
+                    if board[b] > 1:
+                        value = False
+               
+            if value == True:
+                result += 1
 
 print(result)
