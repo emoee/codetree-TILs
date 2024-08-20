b95c6096250d69a591f8828d6bc059ca1ecf34fc
Minsun <elinnane0@gmail.com>
2024-08-21 00:22:03 +0900

    Title: 이상한 폭탄 2, Time: 121ms, Memory: 26MB, Status: Passed - CodeTree

--- 240821/이상한 폭탄 2/strange-bomb-2.py
+++ 240821/이상한 폭탄 2/strange-bomb-2.py
@@ -8,7 +8,7 @@ for i in range(n):
 result = -1
 for i in range(n-1):
     for j in range(i+1, n):
-        if board[i] == board[j] and (j+1) - i >= k:
+        if board[i] == board[j] and j - i <= k:
             result = max(result, board[i])
                 
 print(result)
