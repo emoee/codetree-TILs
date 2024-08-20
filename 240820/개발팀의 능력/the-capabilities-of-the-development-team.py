2fbc541d20a1e56eb4ab03e51f26fce0a6f80438
Minsun <elinnane0@gmail.com>
2024-08-20 23:07:14 +0900

    Title: 개발팀의 능력, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,21 @@
+numbers = list(map(int, input().split()))
+n = 5
+sumNumbers = sum(numbers)
+result = sumNumbers
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            for l in range(k+1, n):
+                teamA = numbers[i] + numbers[j]
+                teamB = numbers[k] + numbers[l]
+                teamC = sumNumbers - (teamA + teamB)
+
+                maxTeam = max(teamA, teamB, teamC)
+                minTeam = min(teamA, teamB, teamC)
+                
+                if teamA != teamB or teamB != teamC or teamA != teamC:
+                    result = min(result, maxTeam-minTeam)
+if result == sumNumbers:
+    result = -1
+
+print(result)
