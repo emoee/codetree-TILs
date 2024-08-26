6fbbe0f860bf0ca45a4fb4d62491482eb51471ef
Minsun <elinnane0@gmail.com>
2024-08-26 16:09:00 +0900

    Title: 훌륭한 점프, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240826/훌륭한 점프/great-jump.py
@@ -0,0 +1,28 @@
+n, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+result = 0
+start = 0
+end = n
+
+def check(numbers, s, n, k):
+    index = s+1
+    if s+k < n:
+        c = numbers[s+1]
+        for i in range(s+1, s+k+1):
+            if numbers[i] < c:
+                index = i
+    else:
+        index = n-1
+    print(index)
+    return index
+
+
+while 1:
+    start = check(numbers, start, n, k)
+    result = max(result, numbers[start])
+
+    if start == n-1:
+        break
+
+print(result)
