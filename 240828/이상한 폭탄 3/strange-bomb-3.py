9ea90bf155f4a3880935d989a9e4e86527f5cf51
Minsun <elinnane0@gmail.com>
2024-08-28 12:42:38 +0900

    Title: 이상한 폭탄 3, Time: 145ms, Memory: 32MB, Status: Wrong Answer - CodeTree

---
+++ 240828/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,23 @@
+n, k = map(int, input().split())
+arr = [
+    int(input()) for _ in range(n)
+]
+result = [0] * 1000000
+
+def in_range(x):
+    return 0 <= x < n
+
+for i in range(n):
+    b = arr[i]
+    for j in range(i+1, i+k+1):
+        if in_range(j) and b == arr[j]:
+            result[b] += 1
+    
+maxValue = max(result)
+
+index = 0
+for i in range(len(result)):
+    if maxValue == result[i]:
+        index = i
+
+print(index)
