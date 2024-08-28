aff4f67e6fb5b3d47beee0e2ff06472305eb4d1d
Minsun <elinnane0@gmail.com>
2024-08-28 22:06:38 +0900

    Title: 최대 최소간의 차, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240828/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -2,8 +2,8 @@ n, k = map(int, input().split())
 numbers = list(map(int, input().split()))
 
 result = 10000 * 10000
-for l in range(min(numbers), max(numbers)-k+1):
-    h = l + 2
+for l in range(1, max(numbers)-k+1):
+    h = l + k
     cost = 0
 
     for n in numbers:
