311067a2618fc39a3c1a794d5815bd75b35c6596
Minsun <elinnane0@gmail.com>
2024-09-02 23:39:45 +0900

    Title: 구역 청소, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/구역 청소/district-cleaning.py
+++ 240902/구역 청소/district-cleaning.py
@@ -1,7 +1,10 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-x = min(a, c)
-y = max(b, d)
+if c < a or b > d:
+    x = min(a, c)
+    y = max(b, d)
 
-print(abs(x-y))
+    print(abs(x-y))
+else:
+    print((b-a) + (d-c))
