b44d3128997c9a600b52886fee3a97c70355bb79
Minsun <elinnane0@gmail.com>
2024-09-03 00:07:31 +0900

    Title: 비둘기와 전기줄, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240903/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240903/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -8,15 +8,16 @@ birds = [0] * 11
 
 lines.sort()
 start = lines[0][0]
-birds[start] = lines[start][1]
+birds[start] = lines[0][1]
 for i in range(1, n):
     if start == lines[i][0]:
         continue
     
-    birds[i] = lines[i][1]
+    birds[lines[i][0]] = lines[i][1]
     start = lines[i][0]
 
 count = 0
+
 for bird, position in lines:
     if birds[bird] != position:
         birds[bird] = position
