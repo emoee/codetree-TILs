3e95e3ae3b69a6a353c88f8334e1251881234bd8
Minsun <elinnane0@gmail.com>
2024-09-03 09:51:00 +0900

    Title: 달리자, Time: 106ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240903/달리자/run-and-run.py
+++ 240903/달리자/run-and-run.py
@@ -7,7 +7,7 @@ result = [0] * 101
 count = 0
 result[0] = listA[0]
 for i in range(n-1):
-    for k in range(1, 101):
+    for k in range(101):
         if k == listB[i]:
             count += result[i] - k
             result[i+1] = listA[i+1] + (result[i] - k)
