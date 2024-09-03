2389e6f3b0fcce5ccf71d9d23aa20e5d1a58815d
Minsun <elinnane0@gmail.com>
2024-09-03 09:51:34 +0900

    Title: 달리자, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/달리자/run-and-run.py
+++ 240903/달리자/run-and-run.py
@@ -7,7 +7,7 @@ result = [0] * 101
 count = 0
 result[0] = listA[0]
 for i in range(n-1):
-    for k in range(101):
+    for k in range(100000):
         if k == listB[i]:
             count += result[i] - k
             result[i+1] = listA[i+1] + (result[i] - k)
