51ee2a73057146c9f4196bd33420f8dba82517bf
Minsun <elinnane0@gmail.com>
2024-09-03 09:49:21 +0900

    Title: 달리자, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240903/달리자/run-and-run.py
@@ -0,0 +1,16 @@
+n = int(input())
+listA = list(map(int, input().split()))
+listB = list(map(int, input().split()))
+
+result = [0] * 101
+
+count = 0
+result[0] = listA[0]
+for i in range(n-1):
+    for k in range(1, 101):
+        if k == listB[i]:
+            count += result[i] - k
+            result[i+1] = listA[i+1] + (result[i] - k)
+            break
+
+print(count)
