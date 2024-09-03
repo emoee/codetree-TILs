3adccbc1d0341f825f022d3d46bb8c46c25569ef
Minsun <elinnane0@gmail.com>
2024-09-04 02:09:02 +0900

    Title: 독서실의 거리두기 2, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240904/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240904/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -28,16 +28,6 @@ def findMin(seat):
     x, y = 0, 0
     for i in range(n-1):
         for j in range(i+1, n):
-            if (seat[i] == '0' and i == 0) and seat[j] == '1':
-                if distance > (j-i):
-                    distance = min(distance, j-i)
-                    x, y = i, j
-                break
-            if seat[i] == '1' and (j == n and seat[j] == '0'):
-                if distance > (j-i):
-                    distance = min(distance, j-i)
-                    x, y = i, j
-                break
             if seat[i] == '1' and seat[j] == '1':
                 if distance > (j-i):
                     distance = min(distance, j-i)
@@ -52,7 +42,7 @@ elif b == n-1 and seat[b] == '0':
     seat[b] = '1'
 else:
     seat[(a+b)//2] = '1'
+# print(seat)
 
 cd, ca, cb = findMin(seat)
-
 print(cd)
