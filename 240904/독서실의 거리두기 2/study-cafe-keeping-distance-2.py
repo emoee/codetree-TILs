4f2b9dc5a16c6b15835be93ec983523a376e5db7
Minsun <elinnane0@gmail.com>
2024-09-04 01:58:34 +0900

    Title: 독서실의 거리두기 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240904/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,45 @@
+n = int(input())
+seat = list(input())
+
+def findMax(seat):
+    distance = 0
+    x, y = 0, 0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if seat[i] == '1' and (j == n-1 and seat[j] == '0'):
+                if distance < (j-i):
+                    distance = max(distance, j-i)
+                    x, y = i, j
+                break
+            if seat[i] == '1' and seat[j] == '1':
+                if distance < (j-i):
+                    distance = max(distance, j-i)
+                    x, y = i, j
+                break
+    return distance, x, y
+
+def findMin(seat):
+    distance = 1000
+    x, y = 0, 0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if seat[i] == '1' and (j == n and seat[j] == '0'):
+                if distance > (j-i):
+                    distance = min(distance, j-i)
+                    x, y = i, j
+                break
+            if seat[i] == '1' and seat[j] == '1':
+                if distance > (j-i):
+                    distance = min(distance, j-i)
+                    x, y = i, j
+                break
+    return distance, x, y
+
+d, a, b = findMax(seat)
+if b == n-1:
+    seat[b] = '1'
+else:
+    seat[(a+b)//2] = '1'
+cd, ca, cb = findMin(seat)
+
+print(cd)
