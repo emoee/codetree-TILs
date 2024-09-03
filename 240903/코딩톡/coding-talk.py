5f6a3a8a773124bee875c1c587836ac4cf430458
Minsun <elinnane0@gmail.com>
2024-09-03 10:22:34 +0900

    Title: 코딩톡, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240903/코딩톡/coding-talk.py
+++ 240903/코딩톡/coding-talk.py
@@ -6,10 +6,16 @@ talk = [
 ]
 check = [0] * 27
 
+before = int(talk[number-2][1])
 for i in range(number-1, 101):
     if i < message:
         c, u = talk[i]
         check[ord(c)-65] = 1
+        if before == int(u):
+            c2, u2 = talk[i-1]
+            check[ord(c2)-65] = 1
+        
+        before = int(u)
     else:
         break
 
