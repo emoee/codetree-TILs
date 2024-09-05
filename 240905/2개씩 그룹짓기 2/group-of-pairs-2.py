1b2d35118814ae3add51e79edcd69c957f8e4d8f
Minsun <elinnane0@gmail.com>
2024-09-05 15:56:09 +0900

    Title: 2개씩 그룹짓기 2, Time: 226ms, Memory: 40MB, Status: Passed - CodeTree

--- 240905/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240905/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -1,9 +1,11 @@
 n = int(input())
 numbers = list(map(int, input().split()))
+numbers.sort()
 
-from itertools import combinations
+result = 1000000000
 
-count = 2*n//n
-print(count)
-for a, b in combinations(numbers, count):
-    print(a,b)
+for i in range((2*n-n)):
+    # print(numbers[n+i]-numbers[i])
+    result = min(result, numbers[n+i]-numbers[i])
+
+print(result)
