89d4e354c7871cf1d2dc448706fa225cc69dedca
Minsun <elinnane0@gmail.com>
2024-09-05 22:11:44 +0900

    Title: A, B, C, D 찾기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240905/A, B, C, D 찾기/find-a-b-c-d.py
+++ 240905/A, B, C, D 찾기/find-a-b-c-d.py
@@ -1,20 +1,9 @@
 numbers = list(map(int, input().split()))
 numbers.sort()
 
-from itertools import combinations
+a = numbers[0]
+b = numbers[1]
+c = numbers[2]
+d = numbers[-1]-(a+b+c)
 
-for a, b, c, d in combinations(numbers, 4):
-    array = [a,b,c,d]
-    for x, y in combinations([a,b,c,d], 2):
-        array.append(x+y)
-    
-    array.append(a+b+c)
-    array.append(a+b+d)
-    array.append(a+c+d)
-    array.append(d+b+c)
-    array.append(a+b+c+d)
-
-    array.sort()
-    if array == numbers:
-        print(a, b, c, d)
-        break
+print(a, b, c, d)
