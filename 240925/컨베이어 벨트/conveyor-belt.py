64f31592a018d42ad240f6a38c28ad20e66087d3
Minsun <elinnane0@gmail.com>
2024-09-25 13:03:36 +0900

    Title: 컨베이어 벨트, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,20 @@
+n, t = map(int, input().split())
+board = [
+    list(map(int, input().split()))
+    for _ in range(2)
+]
+
+first = board[0][n-1]
+second = board[1][n-1]
+
+for i in range(n-1, 0, -1):
+    board[0][i] = board[0][i-1]
+
+for i in range(n-1, 0, -1):
+    board[1][i] = board[1][i-1]
+
+board[0][0] = second
+board[1][0] = first
+
+for row in board:
+    print(" ".join(map(str, row)))
