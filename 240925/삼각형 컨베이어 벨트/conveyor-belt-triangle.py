b62f8874bc37ca55c59da90ae6f3f41dfb1c3a39
Minsun <elinnane0@gmail.com>
2024-09-25 13:25:28 +0900

    Title: 삼각형 컨베이어 벨트, Time: 205ms, Memory: 25MB, Status: Passed - CodeTree

--- 240925/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 240925/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -9,14 +9,10 @@ for _ in range(time):
     second = board[1][n-1]
     third = board[2][n-1]
 
-    for f in range(n-1, 0, -1):
-        board[0][f] = board[0][f-1]
-
-    for s in range(n-1, 0, -1):
-        board[1][s] = board[1][s-1]
-
-    for t in range(n-1, 0, -1):
-        board[2][t] = board[2][t-1]
+    for col in range(n-1, 0, -1):
+        board[0][col] = board[0][col-1]
+        board[1][col] = board[1][col-1]
+        board[2][col] = board[2][col-1]
 
     board[0][0] = third
     board[1][0] = first 
