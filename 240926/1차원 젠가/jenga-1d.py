8a7505163826cfe744f460b6a19650dba73a53a5
Minsun <elinnane0@gmail.com>
2024-09-26 10:23:55 +0900

    Title: 1차원 젠가, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240926/1차원 젠가/jenga-1d.py
@@ -0,0 +1,29 @@
+n = int(input())
+jenga = [
+    int(input()) for _ in range(n)
+]
+
+jenga.reverse()
+arr = [0] * n
+endArray = 0
+endJenga = n
+
+for _ in range(2):
+    s, e = map(int, input().split())
+    for i in range(s-1, e):
+        jenga[i] = 0
+
+    for i in range(endJenga):
+        if jenga[i] != 0:
+            arr[endArray] = jenga[i]
+            endArray += 1
+
+    for i in range(endArray):
+        jenga[i] = arr[i]
+
+    endJenga = endArray
+    endArray = 0
+
+print(endJenga)
+for i in range(endJenga-1, -1, -1):
+    print(jenga[i])
