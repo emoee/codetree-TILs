a612b41f4c7ab94afb58be4528915b02a99556b7
Minsun <elinnane0@gmail.com>
2024-10-01 22:27:36 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 241001/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 241001/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -5,25 +5,23 @@ board = [
 ]
 def inrange(x, y):
     return 0 <= x < n and 0 <= y < n
+    
 dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 std = board[r-1][c-1]
 x, y = r-1, c-1
 
 result = [std]
 while True:
-    px, py = 0, 0
     for dx, dy in zip(dxs, dys):
         nx, ny = dx+x, dy+y
         if inrange(nx, ny) and board[nx][ny] > std:
-            std = board[nx][ny]
-            px, py = nx, ny
+            x, y = nx, ny
             break
 
-    if px == 0 and py == 0:
+    if board[x][y] == std:
         break
 
-    x, y = px, py
-    std = board[px][py]
+    std = board[x][y]
     result.append(std)
 
 print(" ".join(map(str, result)))
