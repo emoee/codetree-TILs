f61dd4c9e1dd81a802c1c8d9bd9141d28439b97f
Minsun <elinnane0@gmail.com>
2024-10-07 15:24:37 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 261ms, Memory: 29MB, Status: Passed - CodeTree

--- 241007/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 241007/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -2,14 +2,15 @@ k, n = map(int, input().split())
 answer = []
 
 def Choose(start):
-    if start == k+1:
+    if len(answer) >= n:
         print(" ".join(map(str, answer)))
         return
     
-    for i in range(1, n+1):
+    for i in range(1, k+1):
         answer.append(i)
         Choose(start+1)
         answer.pop()
+
     return
 
 Choose(1)
