0efb1cf4af611753d266ed660586d1582cbb3fe4
Minsun <elinnane0@gmail.com>
2024-10-08 23:14:55 +0900

    Title: xor 결과 최대 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241008/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,25 @@
+n , m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+
+maxValue = 0
+result = []
+def Choose(cnt, start):
+    global maxValue
+
+    if cnt == m:
+        value = result[0] ^ result[1] ^ result[2]
+        maxValue = max(maxValue, value)
+        return
+    
+    for i in range(start+1, n):
+        result.append(numbers[i])
+        Choose(cnt+1, i)
+        result.pop()
+
+for i in range(n):
+    result.append(numbers[i])
+    Choose(1, i)
+    result.pop()
+
+print(maxValue)
