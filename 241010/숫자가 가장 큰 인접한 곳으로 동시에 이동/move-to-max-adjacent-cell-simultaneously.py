b1eb26c79171a9ad2be6ae6d58f5fe66d61948d1
Minsun <elinnane0@gmail.com>
2024-10-10 12:49:31 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241010/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,43 @@
+n, m, t = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+balls = [[0]*n for _ in range(n)]
+next_balls = [[0]*n for _ in range(n)]
+
+for _ in range(m):
+    r, c = map(int, input().split())
+    balls[r-1][c-1] += 1
+
+def inrange(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def find(x, y):
+    value = board[x][y]
+    dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0]
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+        if inrange(nx,ny) and board[nx][ny] > value:
+            return nx, ny
+    return x, y
+
+def move(x, y):
+    nx, ny = find(x, y)
+    next_balls[nx][ny] += 1
+
+for _ in range(t):
+    next_balls = [[0]*n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if balls[i][j] == 1:
+                move(i, j)
+
+    for i in range(n):
+        for j in range(n):
+            balls[i][j] = next_balls[i][j]
+
+    for i in range(n):
+        for j in range(n):
+            if balls[i][j] >= 2:
+                balls[i][j] = 0
+    
+print(sum(row.count(1) for row in balls))
