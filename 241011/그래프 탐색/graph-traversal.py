3b5addd64e0001549cd7a38654bfcfa3d2b9f91a
Minsun <elinnane0@gmail.com>
2024-10-11 11:05:05 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241011/그래프 탐색/graph-traversal.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+numbers = [[] for _ in range(n+1)]
+visited = [False] * n
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    numbers[x].append(y)
+    numbers[y].append(x)
+
+def dfs(array):
+    global count
+    for value in numbers[array]:
+        if not visited[value]:
+            visited[value] = True
+            dfs(value)
+
+dfs(1)
+print(visited.count(True)-1)
