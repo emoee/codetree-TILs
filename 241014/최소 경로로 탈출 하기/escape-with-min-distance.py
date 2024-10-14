317f273bdf40eb6f216045a80dfdd8a46da7e2ff
Minsun <elinnane0@gmail.com>
2024-10-14 22:27:19 +0900

    Title: 최소 경로로 탈출 하기, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

--- 241014/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 241014/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -22,11 +22,15 @@ def bfs():
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
     while queue:
         x, y = queue.popleft()
-        
+
         for dx, dy in zip(dxs, dys):
             nx, ny = x+dx, y+dy
             if inrange(nx, ny) and not visited[nx][ny] and board[nx][ny] != 0:
                 push(nx, ny, board[x][y]+1)
 
 bfs()
-print(board[n-1][m-1])
+answer = board[n-1][m-1]
+if answer > 1:
+    print(answer)
+else:
+    print(-1)
