0d62bc092625bbf1c8809eb9c60eb862e9bd7d93
Minsun <elinnane0@gmail.com>
2024-10-14 22:25:36 +0900

    Title: 최소 경로로 탈출 하기, Time: 176ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241014/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+n, m = map(int, input().split())
+board = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+visited = [[False]*m for _ in range(n)]
+
+def inrange(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def push(x, y, v):
+    board[x][y] = v
+    visited[x][y] = True
+    queue.append((x, y))
+
+queue = deque()
+push(0, 0, 0)
+
+def bfs():
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+    while queue:
+        x, y = queue.popleft()
+        
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if inrange(nx, ny) and not visited[nx][ny] and board[nx][ny] != 0:
+                push(nx, ny, board[x][y]+1)
+
+bfs()
+print(board[n-1][m-1])
