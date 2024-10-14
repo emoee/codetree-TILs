b743c5a92438fc2ffce8c3a417978ac0a1297979
Minsun <elinnane0@gmail.com>
2024-10-14 22:00:22 +0900

    Title: 갈 수 있는 곳들, Time: 201ms, Memory: 26MB, Status: Passed - CodeTree

--- 241014/갈 수 있는 곳들/places-can-go.py
+++ 241014/갈 수 있는 곳들/places-can-go.py
@@ -10,29 +10,21 @@ def inrange(x, y):
     return 0 <= x < n and 0 <= y < n
 
 def bfs():
-    global road
     while queue:
         x, y = queue.popleft()
-
         dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
         for dx, dy in zip(dxs, dys):
             nx, ny = x+dx, y+dy
             if inrange(nx, ny) and not visited[nx][ny] and board[nx][ny] != 1:    
-                road += 1
                 visited[nx][ny] = True
-                board[nx][ny] = road
                 queue.append((nx,ny))
 
-answer = 0
+queue = deque()
 for i in range(k):
     x, y = map(int, input().split())
-    road = 0
-    queue = deque()
     queue.append((x-1, y-1))
-    if n > 1:
-        bfs()
-    else:
-        road = 1
-    answer += road
+    visited[x-1][y-1] = True
 
+bfs()
+answer = sum(v.count(True) for v in visited)
 print(answer)
