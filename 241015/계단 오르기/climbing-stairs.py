4c2039c5034ed826956f1476d94498fb5a90e7bb
Minsun <elinnane0@gmail.com>
2024-10-15 14:58:42 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241015/계단 오르기/climbing-stairs.py
+++ 241015/계단 오르기/climbing-stairs.py
@@ -4,12 +4,9 @@ dp = [0] * 46
 dp[1] = 1
 dp[2] = 1
 dp[3] = 1
-dp[4] = -1
-dp[5] = 2
-dp[6] = 2
-dp[7] = 3
+dp[4] = 1
 
-for i in range(8, n+1):
+for i in range(5, n+1):
     dp[i] = dp[i-2] + dp[i-3]
 
 print(dp[n])
