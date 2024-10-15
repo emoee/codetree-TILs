d3c5418ac4cc571953daf2042278f6e552539884
Minsun <elinnane0@gmail.com>
2024-10-15 13:03:05 +0900

    Title: 나이트, Time: 203ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241015/나이트/knight-movements.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+board = [[0]*n for _ in range(n)]
+
+def inrange(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def push(x, y, v):
+    board[x][y] = v
+    queue.append((x, y))
+
+queue = deque()
+push(r1-1, c1-1, 0)
+
+def bfs():
+    dxs, dys = [-2, -2, -1, -1, 1, 1, 2, 2], [-1, 1, -2 , 2, -2, 2, -1, 1]
+    while queue:
+        x, y = queue.popleft()
+
+        if x == r2-1 and y == c2-1:
+            break
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if inrange(nx, ny):
+                push(nx, ny, board[x][y]+1)
+
+bfs()
+print(board[r2-1][c2-1])
