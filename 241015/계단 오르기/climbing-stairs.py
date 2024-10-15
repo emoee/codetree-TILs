1a5baec2f83f71b7adf52f052061a6a9d718c6f0
Minsun <elinnane0@gmail.com>
2024-10-15 15:03:18 +0900

    Title: 계단 오르기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/계단 오르기/climbing-stairs.py
+++ 241015/계단 오르기/climbing-stairs.py
@@ -2,12 +2,11 @@ n = int(input())
 MOD = 10007
 
 dp = [0] * max((n+1), 5)
-dp[1] = 1
+dp[1] = 0
 dp[2] = 1
 dp[3] = 1
-dp[4] = 1
 
-for i in range(5, n+1):
+for i in range(4, n+1):
     dp[i] = (dp[i-2] + dp[i-3])%MOD
 
 print(dp[n])
