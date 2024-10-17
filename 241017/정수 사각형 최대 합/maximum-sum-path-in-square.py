a2903554b907a0a39c1f08eb3bb9d68f9a0649dc
Minsun <elinnane0@gmail.com>
2024-10-17 13:53:46 +0900

    Title: 정수 사각형 최대 합, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 241017/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 241017/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -6,7 +6,8 @@ board = [
 dp = [[0]*n for _ in range(n)]
 
 def init():
-    for i in range(n):
+    dp[0][0] = board[0][0]
+    for i in range(1, n):
         dp[i][0] = board[i][0] + dp[i-1][0]
         dp[0][i] = board[0][i] + dp[0][i-1]
 
