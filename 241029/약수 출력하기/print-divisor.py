09f5b5dc3333eefc961f8ac1a5fb4f88835bc478
Minsun <elinnane0@gmail.com>
2024-10-29 11:20:37 +0900

    Title: 약수 출력하기, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 241029/약수 출력하기/print-divisor.py
+++ 241029/약수 출력하기/print-divisor.py
@@ -2,8 +2,11 @@ n = int(input())
 
 result = []
 
-for i in range(1, n+1):
+for i in range(1, int(n**(1/2)) + 1):
     if (n%i) == 0:
         result.append(i)
-
+        if ( (i**2) != n) : 
+            result.append(n // i)
+            
+result.sort()
 print(" ".join(map(str,result)))
