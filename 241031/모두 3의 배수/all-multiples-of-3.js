8bb3a7d36420cbc257e1eb72b04b4230ae5d4f10
Minsun <elinnane0@gmail.com>
2024-10-31 23:39:09 +0900

    Title: 모두 3의 배수, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241031/모두 3의 배수/all-multiples-of-3.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let result= 1;
+
+for (let i = 0; i < 5; i++) {
+    if (input[i]%3!==0)
+        result = 0
+        break
+}
+
+console.log(result)
