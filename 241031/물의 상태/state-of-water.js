4509ff3f2ee31bdc228e7d80004d09ff19f8b9ff
Minsun <elinnane0@gmail.com>
2024-10-31 21:43:48 +0900

    Title: 물의 상태, Time: 165ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241031/물의 상태/state-of-water.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+let water = Number(fs.readFileSync(0).toString().trim());
+
+if (water > 100) {
+    console.log("vapor");
+} else if (water < 0 ){
+    console.log("ice");
+} else {
+    console.log("water")
+}
