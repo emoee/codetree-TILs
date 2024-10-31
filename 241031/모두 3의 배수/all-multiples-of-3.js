cadbb0a2b1c5f54985712785dfcfea50c98402df
Minsun <elinnane0@gmail.com>
2024-10-31 23:41:11 +0900

    Title: 모두 3의 배수, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 241031/모두 3의 배수/all-multiples-of-3.js
+++ 241031/모두 3의 배수/all-multiples-of-3.js
@@ -3,9 +3,10 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 let result= 1;
 
 for (let i = 0; i < 5; i++) {
-    if (input[i]%3!==0)
+    if (input[i]%3!==0){
         result = 0
         break
+    }
 }
 
 console.log(result)
