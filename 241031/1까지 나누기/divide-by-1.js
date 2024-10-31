dd5ea4177ad04bdbd1a73a418f1d26dd12a770ce
Minsun <elinnane0@gmail.com>
2024-10-31 23:15:22 +0900

    Title: 1까지 나누기, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241031/1까지 나누기/divide-by-1.js
+++ 241031/1까지 나누기/divide-by-1.js
@@ -2,9 +2,12 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 let i = 1;
 
-while ((n/i) >= 1) {
+while (1) {
     n = n/i;
-    i++;    
+    i++;
+
+    if ((n/i) <= 1) 
+        break  
 }
 
 console.log(i)
