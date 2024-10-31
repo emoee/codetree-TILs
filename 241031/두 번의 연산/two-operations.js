e702f01d8f08e9cba4cae5543c26f736d04fc36d
Minsun <elinnane0@gmail.com>
2024-10-31 21:49:01 +0900

    Title: 두 번의 연산, Time: 172ms, Memory: 47MB, Status: Passed - CodeTree

--- 241031/두 번의 연산/two-operations.js
+++ 241031/두 번의 연산/two-operations.js
@@ -2,12 +2,10 @@ const fs = require('fs');
 let a = Number(fs.readFileSync(0).toString().trim());
 
 if (a % 2 === 1) {
-  a += 3
+  a = a+3
 }
-
-let result = 0
 if (a % 3 === 0) {
-  result = parseInt(a/3)
+  a = parseInt(a/3)
 }
 
-console.log(result);
+console.log(a);
