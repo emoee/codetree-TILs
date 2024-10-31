1c31cb7cbe3475aa30f64b44abf1b0ab2929905a
Minsun <elinnane0@gmail.com>
2024-10-31 23:37:05 +0900

    Title: 공약수의 유무, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 241031/공약수의 유무/presence-or-absence-of-a-common-divisor.js
+++ 241031/공약수의 유무/presence-or-absence-of-a-common-divisor.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let [a,b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 let result = 0;
 
-for (let i=a; a <= b; i++){
+for (let i=a; i <= b; i++){
     if (1920%i===0 && 2880%i===0) {
         result = 1;
         break
